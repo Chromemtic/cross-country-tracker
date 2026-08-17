164830f76178abdf5a1600d6e96db967138d5a53
Chromemtic <edisonlin870@gmail.com>
2026-08-17 12:55:19 -0400

    Add files via upload

--- cross_country_run_tracker.py
+++ cross_country_run_tracker.py
@@ -2,10 +2,11 @@ run_date = str(input("Today's date: "))
 run_time = float(input("How long was today's run: "))
 type_of_distance = str(input("What type of distance was today's run in: "))
 run_distance = float(input("How much distance was today's run: "))
+training_type = str(input("What is today's training type: "))
 
 calculated_pace = run_time / run_distance
 
-print(f"Date: {run_date} | Time ran for: {run_time} | Distance type: {type_of_distance} | Distance: {run_distance} | Pace: {calculated_pace:.2f}")
+print(f"Date: {run_date} | Time ran for: {run_time} | Distance type: {type_of_distance} | Distance: {run_distance} | Pace: {calculated_pace:.2f} | Training type: {training_type}")
 
 with open("training_database.txt", "a") as file:
-    file.write(f"Date: {run_date} | Time ran for: {run_time} | Distance type: {type_of_distance} | Distance: {run_distance} | Pace: {calculated_pace:.2f}")
+    file.write(f"Date: {run_date} | Time ran for: {run_time} | Distance type: {type_of_distance} | Distance: {run_distance} | Pace: {calculated_pace:.2f} | Training type: {training_type}")
